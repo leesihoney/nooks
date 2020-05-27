8240f5d4ec115d0173826a6ce0382ee921174ac4
= <siheonl@andrew.cmu.edu>
2020-05-27 12:30:08 -0400

    useTabs

--- useInput/index.js
+++ useInput/index.js
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
 
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
 const App = () => {
-  const maxLen = value => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello </h1>
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}> {section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
